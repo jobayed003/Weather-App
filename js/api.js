4aca19f336edb701d3aa0692ded352a648bbe1b7
jobayed003 <jobayed803@gmail.com>
2021-08-28 17:24:26 +0600

    icon change

--- js/api.js
+++ js/api.js
@@ -7,40 +7,6 @@ import {
 } from './helper.js';
 import * as name from './config.js';
 
-// export const weather = async (lat, long) => {
-//   try {
-//     const resLoad = await fetch(
-//       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${name.API_KEY}`
-//     );
-
-//     const data = await resLoad.json();
-
-//     changingTextcontent(data);
-
-//     const secondWeather = async () => {
-//       const res = await fetch(
-//         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${name.API_KEY}`
-//       );
-//       const data = await res.json();
-
-//       changingDetails(data);
-//       if (data) removeSpinner();
-//     };
-//     secondWeather();
-
-//     // Implementing the search results for searchig for location
-//     name.search.addEventListener('submit', e => {
-//       e.preventDefault();
-//       loadingSpinner();
-//       searchResult(name.query.value);
-//       searchWeather(name.query.value);
-//       name.query.value = '';
-//     });
-//   } catch (err) {
-//     loadingSpinner();
-//   }
-// };
-
 export const searchResult = async query => {
   try {
     const res = await fetch(
@@ -67,7 +33,6 @@ export const searchWeather = async query => {
       `https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data[0].lon}&exclude=minutely&appid=${name.API_KEY}`
     );
     const data2 = await res2.json();
-    // if (!changingDetails(data2)) loadingSpinner();
     changingDetails(data2);
     removeSpinner();
   } catch (err) {
